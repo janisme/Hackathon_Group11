fc92cda5eb7061ef6f6d3b414ba0d0cf34276409
Jan <janisme124@gmail.com>
2023-10-07 15:11:01 -0400

    create map, events and likes in models.py

--- start/start/models.py
+++ start/start/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
+class ProblemType(models.TextChoices):
+    CLIMATE_CHANGE = 'Climate Change', 'Climate Change'
+    AIR_POLLUTION = 'Air Pollution', 'Air Pollution'
+    WATER_POLLUTION = 'Water Pollution', 'Water Pollution'
+    DEFORESTATION = 'Deforestation', 'Deforestation'
+    LOSS_OF_BIODIVERSITY = 'Loss of Biodiversity', 'Loss of Biodiversity'
+    WASTE_MANAGEMENT = 'Waste Management', 'Waste Management'
+    OVERFISHING = 'Overfishing', 'Overfishing'
+    RESOURCE_DEPLETION = 'Resource Depletion', 'Resource Depletion'
+
+
 class IssueLog(models.Model):
     username = models.CharField(max_length=50)
     issue_id = models.CharField(max_length=6)
@@ -7,19 +19,33 @@ class IssueLog(models.Model):
     latitude = models.FloatField()
     zipcode = models.IntegerField()
 
-    TYPE_CHOICES = [
-        ('Climate Change', 'Climate Change'),
-        ('Air Pollution', 'Air Pollution'),
-        ('Water Pollution', 'Water Pollution'),
-        ('Deforestation', 'Deforestation'),
-        ('Loss of Biodiversity', 'Loss of Biodiversity'),
-        ('Waste Management', 'Waste Management'),
-        ('Overfishing', 'Overfishing'),
-        ('Resource Depletion', 'Resource Depletion'),
-    ]
-
     typeofproblem = models.CharField(
         max_length=100,
-        choices=TYPE_CHOICES,
-        default='Climate Change'
+        choices=ProblemType.choices,
+        default=ProblemType.CLIMATE_CHANGE
     )
+
+    events = models.CharField(max_length=50)
+
+
+class Event(models.Model):
+    typeofproblem = models.CharField(
+        max_length=100,
+        choices=ProblemType.choices,
+        default=ProblemType.CLIMATE_CHANGE
+    )
+
+    location = models.CharField(max_length=50)
+    time = models.TimeField()
+    date = models.DateField()
+
+class Likes(models.Model):
+    Liking = models.BooleanField()
+    issue_id = models.CharField(max_length=6)
+
+class Map(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    zipcode = models.IntegerField()
+    city = models.CharField(max_length=20)
+
